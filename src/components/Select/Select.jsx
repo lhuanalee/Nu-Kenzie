5746d46b70682b33dd8af8a05213afb4973ff96c
Lhuana Lee <leelhuana@gmail.com>
2022-11-17 00:15:19 -0300

    feat: select

---
+++ src/components/Select/Select.jsx
@@ -0,0 +1,13 @@
+import { SelectStyle } from "./SelectStyle";
+
+const Select = () => {
+  return (
+    <SelectStyle name="" id="">
+      <option value="Select">Select</option>
+      <option value="Entry">Entrada</option>
+      <option value="Expense">Despesa</option>
+    </SelectStyle>
+  );
+};
+
+export default Select;
