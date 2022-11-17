ce299e21581ce4424a7bbf8734a262b967b82a7b
Lhuana Lee <leelhuana@gmail.com>
2022-11-17 00:14:29 -0300

    feat: input

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,12 @@
+const Input = ({ className, id, type, placeholder }) => {
+  return (
+    <input
+      className={className}
+      id={id}
+      type={type}
+      placeholder={placeholder}
+    />
+  );
+};
+
+export default Input;
---
+++ src/components/Input/InputStyle.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const InputStyle = styled.input`
+  padding: 15px;
+
+  border: 2px solid transparent;
+  border-radius: 8px;
+  background: var(--grey-1);
+`;
